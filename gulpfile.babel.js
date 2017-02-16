c88f95a0edde15919e5dc3b48fa56e18a1d7e661
Thomas Grainger <tom@procensus.com>
2017-02-16 18:46:32 +0000

    remove all the version header boilerplate
    users have to include the full MIT license.
    Inlcuding a comment at the top encourages users to shirk that
    responsibility.
    
    also it messes with sourcemaps

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -51,16 +51,6 @@ gulp.task('compile:javascript', ['compile:babel'], () => {
   return gulp.src(['./dist/sc-date-time.js', './dist/*.tpl.temp'])
     .pipe(order(['dist/sc-date-time.js', 'dist/*.tpl.temp']))
     .pipe(concat('sc-date-time.js'))
-    .pipe(concat.header(`/*
-  @license sc-date-time
-  @author SimeonC
-  @license 2015 MIT
-  @version ${pkg.version}
-  
-  See README.md for requirements and use.
-*/\
-`,
-    ))
     .pipe(gulp.dest('dist'));
 });
 
@@ -70,98 +60,10 @@ gulp.task('compile:stylus', ['clean:dist'], () => {
     .pipe(stylus())
     .pipe(autoprefixer())
     .pipe(concat())
-    .pipe(concat.header(`/*
-  @license sc-date-time
-  @author SimeonC
-  @license 2015 MIT
-  @version ${pkg.version}
-  
-  See README.md for requirements and use.
-*/\
-`,
-    ))
     .pipe(rename('sc-date-time.css'))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('compile:main', ['compile:javascript', 'compile:stylus']);
 gulp.task('compile', ['compile:main'], cb => del(['dist/*.temp'], cb));
-
-/*
-  Bumping version number and tagging the repository with it.
-
-  You can use the commands
-
-    gulp prerel    # makes v0.1.0 -> v0.1.1-pre1
-    gulp patch    # makes v0.1.0 → v0.1.1
-    gulp minor    # makes v0.1.1 → v0.2.0
-    gulp major    # makes v0.2.1 → v1.0.0
-
-  To bump the version numbers accordingly after you did a patch,
-  introduced a feature or made a backwards-incompatible release.
-*/
-const releaseVersion = importance =>
-  // get all the files to bump version in
-  gulp.src(['./package.json'])
-    // bump the version number in those files
-    .pipe(bump({ type: importance }))
-    // save it back to filesystem
-    .pipe(gulp.dest('./'))
-;
-gulp.task('tagversion', () =>
-  gulp.src(['./package.json', './changelog.md', './dist/*'])
-    // commit the changed version number
-    .pipe(git.commit('chore(release): Bump Version Number'))
-    // Filter down to only one file
-    .pipe(filter('package.json'))
-    // **tag it in the repository**
-    .pipe(tagVersion()),
-);
-
-gulp.task('changelog', cb => {
-  const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
-  return changelog({
-    version: pkg.version,
-    repository: pkg.repository.url,
-  }
-  , (err, content) => fs.writeFile('./changelog.md', content, cb));
-});
-
-gulp.task('release:prerel', () => releaseVersion('prerelease'));
-gulp.task('release:patch', () => releaseVersion('patch'));
-gulp.task('release:minor', () => releaseVersion('minor'));
-gulp.task('release:major', () => releaseVersion('major'));
-gulp.task('prerel', () =>
-  runSequence(
-    'release:prerel'
-    , 'changelog'
-    , 'compile'
-    , 'tagversion',
-  ),
-);
-gulp.task('patch', () =>
-  runSequence(
-    'release:patch'
-    , 'changelog'
-    , 'compile'
-    , 'tagversion',
-  ),
-);
-gulp.task('minor', () =>
-  runSequence(
-    'release:minor'
-    , 'changelog'
-    , 'compile'
-    , 'tagversion',
-  ),
-);
-gulp.task('major', () =>
-  runSequence(
-    'release:major'
-    , 'changelog'
-    , 'compile'
-    , 'tagversion',
-  ),
-);
-
 gulp.task('default', ['compile']);
